9adaa04243beb574c7f95de92e861c67698ee952
Doug Bromley <doug@tintophat.com>
2020-07-11 18:24:32 +0000

    Basic refactoring and comment improvements

--- display_enviro.py
+++ display_enviro.py
@@ -60,12 +60,12 @@ class ZeroWEnvironment:
         """Change the current view shown to the next
         or previous one shown
         """
-        if direction is "left":
+        if direction == "left":
             if self.current_view != 0:
                 self.current_view = self.current_view - 1
             else:
                 self.current_view = 2
-        if direction is "right":
+        if direction == "right":
             if self.current_view < 2:
                 self.current_view = self.current_view + 1
             else:
@@ -84,19 +84,18 @@ class ZeroWEnvironment:
         elif self.views[self.current_view] == "light":
             self.show_light()
         else:
+            print(self.views[self.current_view])
             self.show_error()
 
     def show_error(self):
-        """
-        Very basic way os stating an error has occured
+        """Very basic way os stating an error has occured
         to the user via the Display-A-Tron
         """
         self.prepare_lcd()
         self.lcd.write("Error in displaying data")
 
     def prepare_lcd(self):
-        """
-        Clear the LCD and set cursor position to o,o of screen
+        """Clear the LCD and set cursor position to o,o of screen
         in preperation for writing data to it.
         """
         self.lcd.set_contrast(0x30)
@@ -104,8 +103,7 @@ class ZeroWEnvironment:
         self.lcd.set_cursor_position(0, 0)
 
     def show_weather(self):
-        """
-        Using the Envirophat sensors display the
+        """Using the Envirophat sensors display the
         temperature and pressure
         """
         self.backlight.set_graph(0.1)
@@ -115,36 +113,36 @@ class ZeroWEnvironment:
         self.lcd.write("%.2f hPA" % self.weather.pressure(unit=self.unit))
 
     def show_gas(self):
+        """Show analog gas readings for MQ135 and MQ9
+        MQ135 is air quality and MQ9 is carbon monoxide
         """
         self.backlight.set_graph(0.2)
         self.prepare_lcd()
         analog_values = self.analog.read_all()
-        mq5 = analog_values[0]
+        mq9 = analog_values[0]
         mq135 = analog_values[1]
-        self.lcd.write("MQ5 = {:f}".format(mq5))
+        self.lcd.write("CO = {:f}".format(mq9))
         self.lcd.set_cursor_position(0, 1)
         self.lcd.write("Air = {:f}".format(mq135))
 
     def show_environment(self):
-        """
-        Show the environment data from CCS811 chip
+        """Show the environment data from CCS811 chip
         connetced via I2c
         """
         self.backlight.set_graph(0.4)
         self.prepare_lcd()
-        self.lcd.write("CO2: %1.0f PPM" % self.ccs811.eco2)
+        self.lcd.write("CO2: {:.3f} PPM".format(self.ccs811.eco2))
         self.lcd.set_cursor_position(0, 1)
-        self.lcd.write("TVOC: {:f} PPM".format(self.ccs811.tvoc))
+        self.lcd.write("TVOC: {:.3f} PPM".format(self.ccs811.tvoc))
         self.lcd.set_cursor_position(0, 2)
-        print("Temp: {:f} C" % self.ccs811.temperature)
+        self.lcd.write("Temp: {:.1f} C".format(self.ccs811.temperature))
 
     def show_light(self):
-        """
-        Show the light sensor readings from the Envirophat
-        """
+        """Show the light sensor readings from the Envirophat"""
+        self.backlight.set_graph(0.6)
         rgb = self.light.rgb()
         self.prepare_lcd()
-        self.lcd.write("Light Level: {:f}".format(self.light.light()))
+        self.lcd.write("Light Level: {:.3f}".format(self.light.light()))
         self.lcd.set_cursor_position(0, 2)
         self.lcd.write("RGB Values: {0}, {1}, {2}".format(rgb[0], rgb[1], rgb[2]))
 
